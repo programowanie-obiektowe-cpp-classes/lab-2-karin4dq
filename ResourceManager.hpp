f94372863b394c8b0518b02619f67d42885fee37
Karina Dudko <133782806+karin4dq@users.noreply.github.com>
2025-10-30 22:34:05 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,45 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource;
+
+public:
+    ResourceManager()
+    {
+        resource = new Resource();
+    }
+
+    ~ResourceManager()
+    {
+        delete resource;
+    }
+
+// konstruktor kopiujący
+// other to referencja_do_obiektu_z_ktorego_kopiuje_dane
+    ResourceManager(const ResourceManager& other)
+    {
+        resource = new Resource(*other.resource);
+    }
+
+// kopiujący operator przypisania
+    ResourceManager& operator=(const ResourceManager& other )
+    {
+        if (this != &other) //ochrona przed samoprzypisaniem
+        {
+        // zwolnienie dotychczasowych zasobów
+        // skopiowanie zasobów z other
+            Resource* nowy_zasob = new Resource(*other.resource);
+            delete resource;
+            resource = nowy_zasob;
+        
+        }
+        return *this;
+    }
+
+    double get() const
+    {
+        return (*resource).get();
+    }
+    
 };
